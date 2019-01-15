c31e3676292ce0aba9e558c22f2be1831136950c
Fedor Indutny <fedor@indutny.com>
2019-01-15 13:27:00 +0100

    model: 3d

--- src/model.py
+++ src/model.py
@@ -9,8 +9,8 @@ class Model:
       x = data
 
       x = self.deconv2d(x, 8, 4, 2, name='encode_1', training=training)
-      x = self.deconv2d(x, 8, 4, 2, name='encode_2', training=training)
-      x = self.deconv2d(x, 16, 4, 2, name='encode_3', training=training)
+      x = self.deconv2d(x, 16, 4, 2, name='encode_2', training=training)
+      # x = self.deconv2d(x, 16, 4, 2, name='encode_3', training=training)
       x = self.deconv2d(x, 1, 1, 1, name='encode_4', training=training,
           activation=tf.nn.sigmoid, bn=False)
 
@@ -27,8 +27,8 @@ class Model:
       x *= contrast * f_training + (1.0 - f_training)
       x += 0.5
 
-      x = self.conv2d(x, 16, 4, 2, name='decode_4', training=training)
-      x = self.conv2d(x, 8, 4, 2, name='decode_3', training=training)
+      # x = self.conv2d(x, 16, 4, 2, name='decode_4', training=training)
+      x = self.conv2d(x, 16, 4, 2, name='decode_3', training=training)
       x = self.conv2d(x, 8, 4, 2, name='decode_2', training=training)
       x = self.conv2d(x, 8, 1, 1, name='decode_1', training=training,
           activation=tf.nn.sigmoid, bn=False)
