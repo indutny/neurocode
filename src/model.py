8ead3e6efca41c3c7bdc0775aee48a9af08dbb4f
Fedor Indutny <fedor@indutny.com>
2019-01-15 13:26:39 +0100

    model: no dropout?

--- src/model.py
+++ src/model.py
@@ -21,13 +21,12 @@ class Model:
       x = image
 
       f_training = tf.cast(training, dtype=tf.float32)
-      contrast = tf.exp(tf.random.normal([ tf.shape(image)[0], 1, 1, 1 ], \
+      contrast = tf.exp(tf.random.normal(tf.shape(x), \
           mean=0.0, stddev=0.223143))
       x -= 0.5
       x *= contrast * f_training + (1.0 - f_training)
       x += 0.5
 
-      x = tf.layers.dropout(x, training=training, rate=0.5)
       x = self.conv2d(x, 16, 4, 2, name='decode_4', training=training)
       x = self.conv2d(x, 8, 4, 2, name='decode_3', training=training)
       x = self.conv2d(x, 8, 4, 2, name='decode_2', training=training)
