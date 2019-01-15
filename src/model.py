b2f7c4d169fef3e9a71cbe358efe96057b6619ae
Fedor Indutny <fedor@indutny.com>
2019-01-15 20:33:58 +0100

    model: just having fun

--- src/model.py
+++ src/model.py
@@ -8,7 +8,7 @@ class Model:
     with tf.variable_scope('neurocode', reuse=tf.AUTO_REUSE, values=[data]):
       x = data
 
-      x = self.deconv2d(x, 32, 3, 1, name='encode_1', training=training)
+      x = self.deconv2d(x, 32, 3, 2, name='encode_1', training=training)
       x = self.deconv2d(x, 16, 3, 2, name='encode_2', training=training)
       x = self.deconv2d(x, 8, 3, 2, name='encode_3', training=training)
       x = self.deconv2d(x, 1, 1, 1, name='encode_4', training=training,
@@ -30,7 +30,7 @@ class Model:
       noise = tf.random.normal(tf.shape(x), stddev=0.3)
       x = x + noise * f_training
 
-      x = self.conv2d(x, 8, 3, 1, name='decode_4', training=training)
+      x = self.conv2d(x, 8, 3, 2, name='decode_4', training=training)
       x = self.conv2d(x, 16, 3, 2, name='decode_3', training=training)
       x = self.conv2d(x, 32, 3, 2, name='decode_2', training=training)
       x = self.conv2d(x, 16, 1, 1, name='decode_1', training=training,
