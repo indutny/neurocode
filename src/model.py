ff7fa8e024518aee73d46b3fe1be77291d98c3bc
Fedor Indutny <fedor@indutny.com>
2019-01-16 10:36:08 +0100

    model: revert to bw

--- src/model.py
+++ src/model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import math
 
 class Model:
   def __init__(self):
@@ -12,18 +11,10 @@ class Model:
       x = self.deconv2d(x, 32, 3, 1, name='encode_1', training=training)
       x = self.deconv2d(x, 16, 3, 2, name='encode_2', training=training)
       x = self.deconv2d(x, 8, 3, 1, name='encode_3', training=training)
-      x = self.deconv2d(x, 2, 1, 1, name='encode_4', training=training,
-          activation=tf.nn.l2_normalize, bn=False)
+      x = self.deconv2d(x, 1, 1, 1, name='encode_4', training=training,
+          activation=tf.nn.sigmoid, bn=False)
 
       return x
-
-  def to_image(self, image):
-    sin, cos = tf.split(image, [ 1, 1 ], axis=-1)
-    angle = tf.math.atan2(sin, cos)
-    hue = (angle / math.pi + 1.0) / 2.0
-    pad = tf.ones_like(hue)
-    hsv = tf.concat([ hue, pad / 1.5, pad ], axis=-1)
-    return tf.image.hsv_to_rgb(hsv)
 
   def decode(self, image, training):
     with tf.variable_scope('neurocode', reuse=tf.AUTO_REUSE, values=[image]):
@@ -31,6 +22,23 @@ class Model:
 
       f_training = tf.cast(training, dtype=tf.float32)
 
+      # Non-normalizing dropout
+      if False:
+        mask = tf.cast(tf.random_uniform(tf.shape(x)) > 0.5, dtype=tf.float32)
+        mask_noise = tf.random_uniform(tf.shape(x))
+        x = f_training * (mask * x  + (1.0 - mask) * mask_noise) + \
+            (1.0 - f_training) * x
+
+      # Contrast
+      contrast = tf.exp(tf.random.normal(tf.shape(x), \
+          mean=0.0, stddev=0.18232155))
+      x -= 0.5
+      x *= contrast * f_training + (1.0 - f_training)
+      x += 0.5
+
+      # Noise
+      noise = tf.random.normal(tf.shape(x), stddev=0.2)
+      x += noise * f_training
 
       x = self.conv2d(x, 8, 3, 1, name='decode_4', training=training)
       x = self.conv2d(x, 16, 3, 2, name='decode_3', training=training)
@@ -64,7 +72,6 @@ class Model:
   def conv2d(self, x, filters, size, strides, name, training,
              activation=tf.nn.relu, bn=True):
     x = tf.layers.conv2d(x, filters=filters, kernel_size=size,
-        padding='SAME',
         strides=(strides, strides), name='{}_conv2d'.format(name))
     if not activation is None:
       x = activation(x)
@@ -73,7 +80,6 @@ class Model:
   def deconv2d(self, x, filters, size, strides, name, training,
       activation=tf.nn.relu, bn=True):
     x = tf.layers.conv2d_transpose(x, filters=filters, kernel_size=size,
-        padding='SAME',
         strides=(strides, strides), name='{}_deconv2d'.format(name))
     if not activation is None:
       x = activation(x)
